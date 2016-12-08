8562c0c91837ada54d1c50e17118693561cc8e5b
Uye121 <yye90012@gmail.com>
2016-12-08 15:31:05 -0800

    Basic Tip Calculator
    Unpolished app

---
+++ Tip Calculator/Tip Calculator/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Ulric Ye on 12/2/16.
+//  Copyright © 2016 uye. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+
+    @IBOutlet weak var tipsBar: UISegmentedControl!
+    @IBOutlet weak var userInput: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        let tipPercentages = [0.18, 0.20, 0.25]
+        
+        let bill = Double(userInput.text!) ?? 0
+        let tip = bill * tipPercentages[tipsBar.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    @IBAction func changeTip(_ sender: Any) {
+        let tipPercentages = [0.18, 0.20, 0.25]
+        
+        let bill = Double(userInput.text!) ?? 0
+        let tip = bill * tipPercentages[tipsBar.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+
+}
+
